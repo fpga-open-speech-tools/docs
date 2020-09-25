446fdd801448de8738d493f8b677a67e8d1ed9c8
Connor Dack <dack@flatearthinc.com>
2020-09-25 12:21:55 -0600

    Initial Commit of Clone Bash Script

---
+++ cloneNIH.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/fpga-open-speech-tools/simulink_models.git
+git clone https://github.com/fpga-open-speech-tools/fpga-open-speech-tools.github.io.git
+git clone https://github.com/fpga-open-speech-tools/utils.git
+git clone https://github.com/fpga-open-speech-tools/arria10_projects.git
+git clone https://github.com/fpga-open-speech-tools/deployment_manager.git
+git clone https://github.com/fpga-open-speech-tools/hardware.git
+git clone https://github.com/fpga-open-speech-tools/simulink_codegen.git
+git clone https://github.com/fpga-open-speech-tools/component_library.git
+git clone https://github.com/fpga-open-speech-tools/max10_projects.git
+git clone https://github.com/fpga-open-speech-tools/de10nano_projects.git
+# git clone https://github.com/fpga-open-speech-tools/docs.git
+git clone https://github.com/fpga-open-speech-tools/autogen_webapp.git
+git clone https://github.com/fpga-open-speech-tools/simulink_library.git
