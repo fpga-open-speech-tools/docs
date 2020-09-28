3ca11b6d4c24a38c7175c51b213fa3e8c5809b17
Connor Dack <dack@flatearthinc.com>
2020-09-28 12:49:37 -0600

    Removed libncurses, added print comments
    Removed Libncurses-dev: This was originally need for the make menuconfig but was replaces with the openspeech_defconfig step
    Added additional echo statements to inform the user of what is going on

--- FrOST_Autogen_Config.sh
+++ FrOST_Autogen_Config.sh
@@ -1,24 +1,26 @@
 #!/bin/sh -e
 
 # Install WSL Packages
+echo "Updating and Installing Packages..."
 sudo apt update;
 sudo apt install gcc-arm-linux-gnueabihf -y;
-# sudo apt install device-tree-compiler -y;
 sudo apt install wsl -y;
 sudo apt install dos2unix -y;
-sudo apt install libncurses-dev -y;
 sudo apt install libssl-dev -y;
 
 # Install the Device Tree Compiler
+echo "Installing the Device Tree Compiler."
 ./docs/scripts/dtc_installer.sh # This will fail to build a Python 2 package but that doesn't matter
 
 # Clone the FrOST Repos
+echo "Cloning the FrOST Git Repos."
 ./docs/scripts/clone_Frost_Repos.sh
 
 # Clone and Configure Linux-SOCFPGA
 cd ~; echo "Moving to Home Directory of WSL."
 git clone https://github.com/fpga-open-speech-tools/linux-socfpga.git;
 cd linux-socfpga;
+echo "make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- openspeech_defconfig"
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- openspeech_defconfig;
 echo "make prepare ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-"
 make prepare ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-;
