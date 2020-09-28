f346e404bc1396af88775f3f4ab28176b4144fff
Connor Dack <dack@flatearthinc.com>
2020-09-28 10:28:50 -0600

    Added WSL Configure and Repo Clone back in

--- FrOST_Autogen_Config.sh
+++ FrOST_Autogen_Config.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -e
 
 # Install WSL Packages
-# sudo apt update;
-# sudo apt install gcc-arm-linux-gnueabihf -y;
-# # sudo apt install device-tree-compiler -y;
-# sudo apt install wsl -y;
-# sudo apt install dos2unix -y;
-# sudo apt install libncurses-dev -y;
-# sudo apt install libssl-dev -y;
+sudo apt update;
+sudo apt install gcc-arm-linux-gnueabihf -y;
+# sudo apt install device-tree-compiler -y;
+sudo apt install wsl -y;
+sudo apt install dos2unix -y;
+sudo apt install libncurses-dev -y;
+sudo apt install libssl-dev -y;
 
-# # Install the Device Tree Compiler
-# ./docs/dtc_installer.sh # This will fail to build a Python 2 package but that doesn't matter
+# Install the Device Tree Compiler
+./docs/dtc_installer.sh # This will fail to build a Python 2 package but that doesn't matter
 
-# # Clone the FrOST Repos
-# ./docs/clone_Frost_Repos.sh
+# Clone the FrOST Repos
+./docs/clone_Frost_Repos.sh
 
 # Clone and Configure Linux-SOCFPGA
 cd ~; echo "Moving to Home Directory of WSL."
